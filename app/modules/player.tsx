d027178323b3039421bfbadff20bf0a6d6629d66
Michael Chan <mijoch@gmail.com>
2025-03-24 17:22:01 -0700

    Use MediaStore React Hook
    https://www.media-chrome.org/docs/en/react/hooks

--- app/modules/player.tsx
+++ app/modules/player.tsx
@@ -1,36 +1,90 @@
 import {
-    MediaController,
-    MediaControlBar,
-    MediaTimeRange,
-    MediaTimeDisplay,
-    MediaVolumeRange,
-    MediaPlayButton,
-    MediaSeekBackwardButton,
-    MediaSeekForwardButton,
-    MediaMuteButton,
-  } from 'media-chrome/react';
-  
-  const Player = () => {
-    return (
-      <MediaController>
-        <video
-          slot="media"
-          src="https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/high.mp4"
-          preload="auto"
-          muted
-          crossOrigin=""
-        />
-        <MediaControlBar>
-          <MediaPlayButton></MediaPlayButton>
-          <MediaSeekBackwardButton></MediaSeekBackwardButton>
-          <MediaSeekForwardButton></MediaSeekForwardButton>
-          <MediaTimeRange></MediaTimeRange>
-          <MediaTimeDisplay showDuration></MediaTimeDisplay>
-          <MediaMuteButton></MediaMuteButton>
-          <MediaVolumeRange></MediaVolumeRange>
-        </MediaControlBar>
-      </MediaController>
-    );
-  };
-  
-  export default Player;
+  MediaProvider,
+  useMediaDispatch,
+  useMediaSelector,
+  useMediaRef,
+  useMediaFullscreenRef,
+  MediaActionTypes,
+} from 'media-chrome/react/media-store';
+
+const Video = () => {
+  // "Wire up" the <video/> element to the MediaStore using useMediaRef()
+  const mediaRef = useMediaRef();
+  return (
+    <video
+      ref={mediaRef}
+      style={{ width: '100vw' }}
+      src="https://stream.mux.com/DS00Spx1CV902MCtPj5WknGlR102V5HFkDe/high.mp4"
+      preload="auto"
+      muted
+      crossOrigin=""
+    />
+  );
+};
+
+const PlayerContainer = ({ children }) => {
+  // "Wire up" the element you want the MediaStore to target for fullscreen using useMediaFullscreenRef()
+  const mediaFullscreenRef = useMediaFullscreenRef();
+  return (<div ref={mediaFullscreenRef}>{children }</div>);
+};
+
+const PlayButton = () => {
+  // Dispatch media state change requests using useMediaDispatch()
+  const dispatch = useMediaDispatch();
+  // Get the latest media state you care about in your component using useMediaSelector()
+  const mediaPaused = useMediaSelector(state => state.mediaPaused);
+  return (
+    <button
+      onClick={() => {
+        // Select from a set of well-defined actions for state change requests
+        // using MediaActionTypes
+        const type = mediaPaused
+          ? MediaActionTypes.MEDIA_PLAY_REQUEST
+          : MediaActionTypes.MEDIA_PAUSE_REQUEST;
+        dispatch({ type });
+      }}
+    >
+      {mediaPaused ? 'Play' : 'Pause'}
+    </button>
+  );
+};
+
+const FullscreenButton = () => {
+  // Dispatch media state change requests using useMediaDispatch()
+  const dispatch = useMediaDispatch();
+  // Get the latest media state you care about in your component using useMediaSelector()
+  const mediaIsFullscreen = useMediaSelector(state => state.mediaIsFullscreen);
+  return (
+    <button
+      onClick={() => {
+        // Select from a set of well-defined actions for state change requests
+        // using MediaActionTypes
+        const type = mediaIsFullscreen
+          ? MediaActionTypes.MEDIA_EXIT_FULLSCREEN_REQUEST
+          : MediaActionTypes.MEDIA_ENTER_FULLSCREEN_REQUEST;
+        dispatch({ type });
+      }}
+    >
+      {mediaIsFullscreen ? 'Exit fullscreen' : 'Enter fullscreen'}
+    </button>
+  );
+};
+
+const Player = () => {
+  // Get access to Media Chrome's state management in your components using <MediaProvider/>
+  // NOTE: Unlike many other providers (including react-redux's Provider), you'll likely want to keep
+  // your <MediaProvider/> in or close to your <Player/> component)
+  return (
+    <MediaProvider>
+      <PlayerContainer>
+        <Video />
+        <div>
+          <PlayButton />
+          <FullscreenButton />
+        </div>
+      </PlayerContainer>
+    </MediaProvider>
+  );
+};
+
+export default Player;
